976540281bc019b64bf8e202f33083c9170d8f3a
Ephemeral <90786729+Nadim147c@users.noreply.github.com>
2024-06-13 08:00:23 +0000

    improve ffmpeg progress for ffmpeg commands

--- api/ffmpeg.mjs
+++ api/ffmpeg.mjs
@@ -1,45 +1,16 @@
 import logUpdate from "log-update"
-import "zx/globals"
 
 export default class Ffmpeg {
     constructor() {}
 
     /**
      * @param {import("zx").ProcessPromise} ffmpegProcess Standard output
-     * @param {minimist.ParsedArgs} argv Parsed arguments of process
+     * @param {string[]} args Parsed arguments of process
      */
-    static async progress(ffmpegProcess, argv) {
+    async progress(ffmpegProcess, args = process.argv) {
         let totalFrames
-        let streamFPS
-        let duration
-
-        /**
-         * @param {string} duration
-         * @returns {number}
-         */
-        const durationToSeconds = (duration) => {
-            const pieces = duration.split(":").reverse()
-            let seconds = 0
-            for (let i = 0; i < pieces.length; i++) {
-                seconds += parseFloat(pieces[i]) * 60 ** i
-            }
-            return seconds
-        }
-
-        if (argv.t && argv.o) {
-            const toIndex = process.argv.indexOf("-to")
-            if (toIndex !== -1 && process.argv.at(toIndex + 1)) {
-                duration = process.argv.at(toIndex + 1)
-
-                const startIndex = process.argv.indexOf("-ss")
-                if (startIndex !== -1 && process.argv.at(startIndex + 1)) {
-                    const start = process.argv.at(startIndex + 1)
-                    duration = (durationToSeconds(duration) - durationToSeconds(start)).toString()
-                }
-            }
-        } else if (typeof argv.t === "string" || typeof argv.t === "number") {
-            duration = `${argv.t}`
-        }
+        let streamFPS = this.parseFrameRateFromArgv(args)
+        let duration = this.parseDurationFromArgv(args)
 
         for await (const chunk of ffmpegProcess.stdout) {
             /** @type {string} */
@@ -48,7 +19,7 @@ export default class Ffmpeg {
             const OVERWRITE_REGEX = /File .+ already exists\. Overwrite\? \[y\/N\]/
             const overwrite = data.match(OVERWRITE_REGEX)
             if (overwrite) {
-                const str = overwrite.at(0).trim()
+                const str = overwrite.at(0)?.trim()
                 process.stdout.write(`${str} `)
                 continue
             }
@@ -57,19 +28,21 @@ export default class Ffmpeg {
             if (!duration) duration = data.match(DURATION_REGEX)?.[1]
 
             const FPS_REGEX = /(\d{2}\.\d{2}|\d{2}) fps/
-            if (!streamFPS) streamFPS = data.match(FPS_REGEX)?.[1]
+            if (!streamFPS) {
+                const fpsMatch = data.match(FPS_REGEX)?.at(1)
+                if (fpsMatch) streamFPS = parseFloat(fpsMatch)
+            }
 
             if (duration && streamFPS && !totalFrames) {
                 // prettier-ignore
-                const seconds = durationToSeconds(duration)
-                totalFrames = seconds * parseFloat(streamFPS)
-                console.log({ seconds, totalFrames })
+                const seconds = this.durationToSeconds(duration)
+                totalFrames = seconds * streamFPS
             }
 
             const PROGRESS_REGEX = /frame= *(\d+) *fps= *(\d+).*speed=(\d+|\d+\.\d+)x/i
             const ffmpegProgress = data.match(PROGRESS_REGEX)
 
-            if (!ffmpegProgress || !totalFrames) continue
+            if (!ffmpegProgress || !totalFrames || !streamFPS) continue
 
             const frames = parseFloat(ffmpegProgress[1])
             const fps = parseFloat(ffmpegProgress[2])
@@ -96,5 +69,91 @@ export default class Ffmpeg {
         }
 
         logUpdate.done()
+    }
+
+    /**
+     * @param {string[]} args minimist parsed arguments
+     * @returns {number|undefined} Parsed fps
+     */
+    parseFrameRateFromArgv(args) {
+        let inputFPS
+
+        const fpsMaxIndex = args.indexOf("-fpsmax")
+        if (fpsMaxIndex !== -1) inputFPS = args.at(fpsMaxIndex + 1)
+
+        const fpsIndex = args.indexOf("-r")
+        if (fpsIndex !== -1) inputFPS = args.at(fpsIndex + 1)
+
+        const filterGraphIndex = args.indexOf("-vf")
+        if (filterGraphIndex !== -1) {
+            const filterGraph = args.at(filterGraphIndex + 1)
+            if (filterGraph && filterGraph.includes("fps=")) inputFPS = filterGraph.match(/fps=(\d+)/)?.at(1)
+        }
+
+        const filterComplexIndex = args.indexOf("-filter_complex")
+        if (filterComplexIndex !== -1) {
+            const filterComplex = args.at(filterComplexIndex + 1)
+            if (filterComplex && filterComplex.includes("fps=")) inputFPS = filterComplex.match(/fps=(\d+)/)?.at(1)
+        }
+
+        if (!inputFPS) return
+
+        if (typeof inputFPS === "number") return inputFPS
+
+        if (!isNaN(parseFloat(inputFPS))) {
+            return parseFloat(inputFPS)
+        }
+
+        if (inputFPS.includes("/")) {
+            const parts = inputFPS.split("/")
+            if (parts.length === 2 && !isNaN(parseInt(parts[0])) && !isNaN(parseInt(parts[1]))) {
+                return parseFloat(parts[0]) / parseFloat(parts[1])
+            }
+        }
+
+        const frameRateAbbreviations = {
+            ntsc: 30000 / 1001,
+            pal: 25,
+        }
+        const abbreviation = inputFPS.toLowerCase()
+        if (abbreviation in frameRateAbbreviations) {
+            // @ts-ignore
+            return frameRateAbbreviations[abbreviation]
+        }
+    }
+
+    /**
+     * @param {string | number} duration Duration value
+     * @returns {number} Parsed duration in seconds
+     */
+    durationToSeconds(duration) {
+        if (typeof duration === "number") return duration
+        const pieces = duration.split(":").reverse()
+        let seconds = 0
+        for (let i = 0; i < pieces.length; i++) {
+            seconds += parseFloat(pieces[i]) * 60 ** i
+        }
+        return seconds
+    }
+
+    /**
+     * @param {string[]} args Parsed arguments by minimist
+     * @returns {string | undefined} Parse duration from arguments
+     */
+    parseDurationFromArgv(args) {
+        const timeIndex = args.indexOf("-t")
+        if (timeIndex !== -1) return args.at(timeIndex + 1)
+
+        const toIndex = args.indexOf("-to")
+        if (toIndex !== -1) {
+            if (toIndex === -1 || !args.at(toIndex + 1)) return
+            const duration = args.at(toIndex + 1)
+
+            const startIndex = args.indexOf("-ss")
+            if (startIndex === -1 || !args.at(startIndex + 1)) return duration
+
+            const start = args.at(startIndex + 1)
+            if (start && duration) return (this.durationToSeconds(duration) - this.durationToSeconds(start)).toString()
+        }
     }
 }
