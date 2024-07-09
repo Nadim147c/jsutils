9d5a1f9214f2ab9099b34c210d63bbb16ac32439
Ephemeral <90786729+Nadim147c@users.noreply.github.com>
2024-07-09 23:31:01 +0600

    add better args parsing to audio command

--- src/yt-dlp/audio.ts
+++ src/yt-dlp/audio.ts
@@ -1,33 +1,53 @@
 #!/usr/bin/env zx
 
 import os from "os"
+import { z } from "zod"
 import "zx/globals"
+import Arguments from "../api/arguments.js"
 import Help from "../api/help.js"
 
-const argv = minimist(process.argv.slice(3), {
-    alias: { help: ["h"] },
-    boolean: ["help", "sponsorblock", "debug"],
+const exts = [
+    z.literal("aac"),
+    z.literal("alac"),
+    z.literal("flac"),
+    z.literal("m4a"),
+    z.literal("mp3"),
+    z.literal("opus"),
+    z.literal("vorbis"),
+    z.literal("wav"),
+] as const
+const args = new Arguments({
+    _: z.array(z.string()).min(1, "Missing argument URL. Please provide an url."),
+    __: z.array(z.string()).optional(),
+    ext: z.union(exts).default("mp3"),
+    section: z.string().optional(),
+    cookies: z.string().optional(),
+    browser: z.string().optional(),
+    threads: z.number().int().min(1).default(1),
+    sponsorblock: z.boolean(),
+    help: z.boolean().optional(),
+    debug: z.boolean().optional(),
 })
 
-if (argv.help) {
-    const helper = new Help("Usage: video [OPTIONS] URL")
-    helper.argument("URL", "url of the video to download")
-    helper
-        .option("--section REGEX", "Section of the video to download. Timestamp must start with * (ex: '*0.11-0.50')")
-        .option("-c, --cookies FILE", "Netscape formatted file to read cookies from and dump cookie jar in")
-        .option("--browser BROWSER", "Name of the browser to use cookies from")
-        .option("--threads NUMBER", "Number of concurrent downloads. Default is 1")
-        .option("--no-sponsorblock", "Disable sponsorblock for downloaded video. Default is on")
-        .option("-h, --help", "Prints the help menu")
-        .option("--debug", "Prints the debug info")
+const helper = new Help("Usage: video [OPTIONS] URL")
+helper.argument("URL", "url of the video to download")
+helper
+    .option("--section REGEX", "Section of the video to download. Timestamp must start with * (ex: '*0.11-0.50')")
+    .option("-c, --cookies FILE", "Netscape formatted file to read cookies from and dump cookie jar in")
+    .option("--browser BROWSER", "Name of the browser to use cookies from")
+    .option("--threads NUMBER", "Number of concurrent downloads. Default is 1")
+    .option("--no-sponsorblock", "Disable sponsorblock for downloaded video. Default is on")
+    .option("-h, --help", "Prints the help menu")
+    .option("--debug", "Prints the debug info")
 
-    echo($({ input: helper.toString(), sync: true })`cm`)
-    process.exit(0)
-}
+const argv = args.parse(helper, {
+    alias: { h: "help" },
+    boolean: ["help", "sponsorblock", "debug"],
+    default: { sponsorblock: true },
+})
 
 if (typeof argv.sponsorblock === "undefined") argv.sponsorblock = true
 if (typeof argv.threads === "undefined") argv.threads = 1
-if (typeof argv.ext === "undefined") argv.ext = "mp3"
 
 if (argv.debug) console.log(argv)
 
@@ -54,14 +74,9 @@ if (argv.section) ytDlpArgs.push("--download-sections", argv.section)
 if (argv.cookies) ytDlpArgs.push("--cookies", argv.cookies)
 if (argv.browser) ytDlpArgs.push("--cookies-from-browser", argv.browser)
 
-let url = argv.url ?? argv._[0]
-if (!url) {
-    console.log(chalk.red("Please provide a url or search term"))
-    process.exit(1)
-}
-
+let url = argv._[0]
 try {
-    url = new URL(url)
+    url = new URL(url).toString()
 } catch (err) {
     url = `ytsearch:${url}`
 }
